d4d534c1eab4498559e5bd4d16b25168a9b0d5cb
BrunoCarvalho18 <brunocarvalhodesa@gmail.com>
2021-07-11 18:22:06 -0300

    primeiro commit

---
+++ test/sum_list_test.exs
@@ -0,0 +1,19 @@
+defmodule SumListTest do
+  use ExUnit.Case
+
+  describe "call/1" do
+    test "retun the list sum" do
+      list = [1,2,3]
+
+      response = SumList.call(list)
+
+      expect_response = 6
+
+      assert response == expect_response
+
+    end
+
+  end
+
+
+end
